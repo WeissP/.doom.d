8895deedee7cf978a8f89e4b3193b7c6b6cbd68d
weiss <kingbaiing@163.com>
2019-10-01 16:35:53 +0200

    update

--- config.el
+++ config.el
@@ -22,10 +22,57 @@
 
 
 (after! org
-  (set-face-attribute 'org-headline-done nil
+  (set-face-attribute 'bold nil
+                      :weight 'bold
+                      :underline 'nil
+                      :foreground "#f5355e"
+                      :background nil)
+  (set-face-attribute 'italic nil
+                      :weight 'normal
+                      :underline 'nil
+                      :slant 'italic
                       :height 0.8
+                      :foreground "#c0c0c0"
+                      :background nil)
+  (set-face-attribute 'org-link nil
+                      :weight 'normal
+                      :underline 'nil
+                      :foreground "#20B2AA"
+                      :background nil)
+  (set-face-attribute 'org-headline-done nil
                       :strike-through t
                       :weight 'normal)
+  (set-face-attribute 'org-level-1 nil
+                      :height 1.2
+                      :foreground "#ff5a19"
+                      :weight 'bold)
+  (set-face-attribute 'org-level-2 nil
+                      :height 1.1
+                      :foreground "#040404"
+                      :weight 'normal)
+  (set-face-attribute 'org-level-3 nil
+                      :height 1.0
+                      :foreground "#040404"
+                      :weight 'normal)
+  (set-face-attribute 'org-level-4 nil
+                      :height 1.0
+                      :foreground "#040404"
+                      :weight 'normal)
+  (set-face-attribute 'org-level-5 nil
+                      :height 1.0
+                      :foreground "#040404"
+                      :weight 'normal)
+  (set-face-attribute 'org-level-6 nil
+                      :height 1.0
+                      :foreground "#040404"
+                      :weight 'normal)
+  (set-face-attribute 'org-level-7 nil
+                      :height 1.0
+                      :foreground "#040404"
+                      :weight 'normal)
+
+  ;; (setq line-spacing 1.5)
+
   (map! :map org-mode-map
       :n "M-j" #'org-metadown
       :n "M-k" #'org-metaup
@@ -36,6 +83,9 @@
                         (67 :foreground "#0098dd")
                         )
    org-directory "~/Dokumente/Org/"
+   ;; org-agenda-files "~/Dokumente/Org/*.org"
+   org-agenda-files (list org-directory)
+   ;; org-agenda-files (ignore-errors (directory-files +org-dir t "\\.org$" t))
    org-todo-keywords '((sequence "TODO(t)" "INPROGRESS(i)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c@)"))
    )
 
@@ -49,6 +99,7 @@
   (setq org-agenda-compact-blocks t)
   (map! :desc "Create Sparse Tree for Tags" :ne "SPC r t" #'org-tags-sparse-tree)
   (map! :desc "switch-and-bookmarks-suchen" :ne "SPC r r" #'switch-and-bookmarks-suchen())
+  (map! :desc "pinyin-search" :ne "SPC r s" #'pinyin-search)
   ;----------------------------------------------------- switch and Bookmarks suchen
  (setq org-agenda-custom-commands '(("b" occur-tree ":Bookmarks:")))
   (fset 'switch-and-bookmarks-suchen
@@ -67,7 +118,7 @@
    )
   ;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-;; (find-file "/home/weiss/Dokumente/Org/Kenntnisse.org")
+(find-file "/home/weiss/Dokumente/Org/Kenntnisse.org")
 (find-file "/home/weiss/Dokumente/Org/todo.org")
 (find-file "/home/weiss/Dokumente/Org/Einsammlung.org")
 (find-file "/home/weiss/Dokumente/Org/Zusammenhang.org")
